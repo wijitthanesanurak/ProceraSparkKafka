c132d96a04c45b38d649e5b8922f5be0d13a23b4
wijit <wijit@DESKTOP-2BT890K>
2017-09-18 10:11:29 +0700

    First Commit

---
+++ SparkPoceraKafka/build.sbt
@@ -0,0 +1,3 @@
+name := "SparkPoceraKafka"
+version := "1.1"
+scalaVersion := "2.11.8"
---
+++ SparkPoceraKafka/src/main/scala/HelloWorld.scala
@@ -0,0 +1,9 @@
+object HelloWorld {
+
+  def main(args: Array[String]) {
+
+    println("Hello world")
+
+  }
+
+}
